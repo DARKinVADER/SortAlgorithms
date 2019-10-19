2c0c3ca9dbdbbd96f1f03ff185a997b72067a528
Gábor Czibik <gabor@czibik.hu>
2019-10-19 17:46:04 +0200

    QuickSort

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -7,12 +7,17 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bubble Sort");
             int[] arrayToSort = new int[] { 5, 1, 4, 2, 8 };
-
             ISortBehavior sort = new BubbleSort();
-
             sort.Sort(arrayToSort);
             arrayToSort.ToList().ForEach(t => Console.WriteLine(t));
+
+            Console.WriteLine("Qucik Sort");
+            int[] arrayToQuickSort = new int[] { 10, 7, 8, 9, 1, 5 };
+            ISortBehavior quickSort = new QuickSort();
+            quickSort.Sort(arrayToQuickSort);
+            arrayToQuickSort.ToList().ForEach(t => Console.WriteLine(t));
         }
     }
 }
---
+++ BubbleSort/QuickSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort
+{
+    class QuickSort : ISortBehavior
+    {
+        public void Sort(int[] array)
+        {
+            QuickSortAlgoritm(array, 0, array.Length - 1);
+        }
+
+        private void QuickSortAlgoritm(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(array, low, high);
+
+                QuickSortAlgoritm(array, low, pi - 1);
+                QuickSortAlgoritm(array, pi + 1, high);
+            }
+        }
+
+        private int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, i + 1, high);
+
+            return i + 1;
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
