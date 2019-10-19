dd695047924bd04cdb6020698efe0c756b860fcc
Gábor Czibik <gabor@czibik.hu>
2019-10-19 17:18:59 +0200

    Add project files.

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort
+{
+    class BubbleSort : SortBehavior
+    {
+        public void Sort(int[] array)
+        {
+            bool sorted = false;
+
+            while (!sorted)
+            {
+                sorted = true;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        sorted = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayToSort = new int[] { 5, 1, 4, 2, 8 };
+
+            SortBehavior sort = new BubbleSort();
+
+            sort.Sort(arrayToSort);
+            arrayToSort.ToList().ForEach(t => Console.WriteLine(t));
+        }
+    }
+}
